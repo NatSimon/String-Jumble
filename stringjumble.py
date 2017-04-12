731b828b45769ccd3ac73d6c750d5e5b2e55683e
NatSimon <nathalieosimon@gmail.com>
2017-04-12 13:45:51 -0400

    Updated from Brython Server: 4/12/2017 1:45:51 PM

--- stringjumble.py
+++ stringjumble.py
@@ -24,6 +24,7 @@ You entered "There are a few techniques or tricks that you may find handy". Now 
 ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
-"""
-phrase= input("Please enter a string of text "):
-print phrase[::-1]
+""" 
+input phrase[("Please enter a string of text ")]
+input = 
+print (x)
