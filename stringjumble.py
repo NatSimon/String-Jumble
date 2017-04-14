a3853a1d94969f07e4ad9e467de656fcc4f2646b
NatSimon <nathalieosimon@gmail.com>
2017-04-14 14:44:16 -0400

    Updated from Brython Server: 4/14/2017 2:44:16 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,6 +25,5 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """ 
-input(list(["Please enter a string of text "]))
-list = x
-print (x)
+string= list(input(["Please enter a string of text "]))
+print (string)
