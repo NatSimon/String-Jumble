ed732656f1b5b9c60e9ed8c1f24f0a9d80800d45
NatSimon <nathalieosimon@gmail.com>
2017-04-24 14:38:14 -0400

    Updated from Brython Server: 4/24/2017 2:38:14 PM

--- stringjumble.py
+++ stringjumble.py
@@ -25,6 +25,8 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """ 
+import string
+string.ascii_lowercase
 string= list(input(["Please enter a string of text "]))
 print (string)
 def shuffle(string):
