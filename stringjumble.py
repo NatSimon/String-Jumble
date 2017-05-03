6034740eb1771488c875234dc2a18c3f9b3a9339
NatSimon <nathalieosimon@gmail.com>
2017-05-03 13:41:54 -0400

    Updated from Brython Server: 5/3/2017 1:41:54 PM

--- stringjumble.py
+++ stringjumble.py
@@ -31,7 +31,7 @@ string= list(input(["Please enter a string of text "]))
 print (string)
 def shuffle(string):
     if len(string) == 1:
-        else:
+     else:
         half = int(len(string) / 2)
      
         first = list[:half][::-1]
